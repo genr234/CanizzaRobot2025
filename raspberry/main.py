8b8f8cadde6d796ad1473c6a08b47b61088d84f5
Giulio Finocchiaro - MacBook Pro <giulfinocchiaro@icloud.com>
2025-05-06 13:08:46 +0200

    Aggiornamento per test

--- raspberry/main.py
+++ raspberry/main.py
@@ -322,9 +322,10 @@ def main_execution():
     """
 
     try:
-        servo_gabbia.set_angle(180)
-        sleep(2)
-        prendi_oggetto()
+        #servo_gabbia.set_angle(180)
+        #sleep(2)
+        #prendi_oggetto()
+        robot.muovi_avanti_for("seconds", 2)
         robot.gira_destra(180, 50)
         robot.muovi_indietro_for("rotations", 0.5)
         #robot.gira_destra(40, 40)
